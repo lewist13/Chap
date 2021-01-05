568f53cad9a9a2486e18cb34e924ade55ed320d2
lewist13 <lewistpro@gmail.com>
2021-01-05 16:45:23 -0500

    built pubmessagescontroller w/auth

---
+++ controllers/UserMessagesPubController.js
@@ -0,0 +1,62 @@
+const { Message } = require("../models");
+const { Op, literal, fn, col } = require("sequelize");
+
+const GetMessages = async (req, res) => {
+  try {
+    let id = parseInt(req.params.message_id);
+    let message = await Message.findAll({ where: { message_id: id } });
+    res.send(message);
+  } catch (error) {
+    throw error;
+  }
+};
+
+const CreateMessage = async (req, res) => {
+  try {
+    const message = await Message.create(req.body);
+    res.send(message);
+  } catch (error) {
+    throw error;
+  }
+};
+
+const UpdateMessage = async (req, res) => {
+  try {
+    // let messageId = parseInt(req.params.channel_id);
+    let updatedMessage = await Message.update(req.body, {
+      //   where: {
+      //     id: channelId,
+      //   },
+      returning: true,
+    });
+    res.send(updatedMessage);
+  } catch (error) {
+    throw error;
+  }
+};
+
+const DeleteMessage = async (req, res) => {
+  try {
+    await Message.destroy({
+      //   where: {
+      //     id: req.params.channel_id,
+      //   },
+    });
+    res.send({
+      message: `Message deleted`,
+      //   data: {
+      //     id: req.params.channel_id,
+      //   },
+    });
+  } catch (error) {
+    // console.log("ID", channel_id);
+    throw error;
+  }
+};
+
+module.exports = {
+  DeleteMessage,
+  UpdateMessage,
+  GetMessages,
+  CreateMessage,
+};
