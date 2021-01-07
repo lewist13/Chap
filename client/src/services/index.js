52d78edb9a58eb94494df9d2a9003cad6bdc6199
lewist13 <lewistpro@gmail.com>
2021-01-07 11:36:21 -0500

    completed all services; moved each to one file

---
+++ client/src/services/index.js
@@ -0,0 +1,197 @@
+import axios from "axios";
+
+const ApiClient = axios.create({
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? `${window.location.origin}/api`
+      : "https://localhost:3000/api",
+});
+
+ApiClient.interceptors.request.use(
+  async (config) => {
+    const token = localstorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (err) => Promise.reject(err)
+);
+
+import ApiClient from "./ApiClient";
+
+export const __CreateUser = async (formData) => {
+  try {
+    const res = await ApiClient.post("/users/add", formData);
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const __GetUsers = async () => {
+  try {
+    const res = await ApiClient.get(`/users/all`);
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const __GetUserById = async (user_id) => {
+  try {
+    const res = await ApiClient.get(`/users/${user_id}`);
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const __Login = async (userData) => {
+  try {
+    const res = await ApiClient.post("/users/login", userData);
+    localstorage.setItem("token", res.data.token);
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const __UpdateUser = async (user_id) => {
+  try {
+    const res = await ApiClient.put(`/users/update/${user_id}`);
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const __DeleteUser = async (user_id) => {
+  try {
+    const res = await ApiClient.delete(`/users/delete/${user_id}`);
+    return res;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const __SessionStatus = async () => {
+  try {
+    const res = await ApiClient.post("/users/session");
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+import ApiClient from "./ApiClient";
+
+export const __CreateServer = async (formData) => {
+  try {
+    const res = await ApiClient.post("/servers/add", formData);
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const __GetServerById = async (server_id) => {
+  try {
+    const res = await ApiClient.get(`/servers/${server_id}`);
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const __UpdateServer = async (server_id) => {
+  try {
+    const res = await ApiClient.put(`/servers/update/${server_id}`);
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const __DeleteServer = async (server_id) => {
+  try {
+    const res = await ApiClient.delete(`/servers/delete/${server_id}`);
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const __CreateChannel = async (formData) => {
+  try {
+    const res = await ApiClient.post("/channels/add", formData);
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const __GetChannels = async (channel_id) => {
+  try {
+    const res = await ApiClient.get(`/channels/${channel_id}`);
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const __UpdateChannel = async (channel_id) => {
+  try {
+    const res = await ApiClient.put(`/channels/udpate/${channel_id}`);
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const __DeleteChannel = async (channel_id) => {
+  try {
+    const res = await ApiClient.delete(`/channels/delete/${channel_id}`);
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const __CreateMessage = async (formData) => {
+  try {
+    const res = await ApiClient.post("/messages/add", formData);
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const __GetMessages = async (message_id) => {
+  try {
+    const res = await ApiClient.get(`/messages/${message_id}`);
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const __UpdateMessage = async (message_id) => {
+  try {
+    const res = await ApiClient.put(`/messages/${message_id}`);
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const __DeleteMessage = async (message_id) => {
+  try {
+    const res = await ApiClient.delete(`/messages/delete/${message_id}`);
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export default ApiClient;
