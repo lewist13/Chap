5b74797ec9cf63749d67d923d47838757d022cbd
lewist13 <lewistpro@gmail.com>
2021-01-07 15:13:34 -0500

    built baseline for the nav bar

---
+++ client/src/components/Nav.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Nav({ authenticated, currentUser, logout }) {
+  return !authenticated && !currentUser ? (
+    <div>
+      <nav>
+        <ul>
+          <Link to="/"></Link>
+          <Link to="/signup">
+            <li>Sign Up</li>
+          </Link>
+          <Link to="/login">
+            <li>Login</li>
+          </Link>
+        </ul>
+      </nav>
+    </div>
+  ) : (
+    <div>
+      <nav>
+        <ul>
+          <Link to="/"></Link>
+          <Link to="/" onClick={(e) => logout(e)}>
+            <li>Logout</li>
+          </Link>
+        </ul>
+      </nav>
+    </div>
+  );
+}
