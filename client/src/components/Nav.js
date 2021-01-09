48cd50433df6f086a2a3e9e8c31f888a5704ac8b
lewist13 <lewistpro@gmail.com>
2021-01-08 19:02:24 -0500

    added assets/materialize

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,18 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import M from "materialize-css/dist/js/materialize.min.js";
+import "materialize-css";
 
 export default function Nav({ authenticated, currentUser, logout }) {
   return !authenticated && !currentUser ? (
     <div>
       <nav>
-        <ul>
-          <Link to="/"></Link>
-          <Link to="/signup">
-            <li>Sign Up</li>
-          </Link>
-          <Link to="/login">
-            <li>Login</li>
-          </Link>
+        <a class="dropdown-trigger btn" href="#" data-target="dropdown1">
+          <img src="./assets/dropdown2.png" />
+        </a>
+        <ul id="dropdown1" class="dropdown-content">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/signup">Sign Up</Link>
+          </li>
+          <li class="divider" tabindex="-1"></li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
         </ul>
       </nav>
     </div>
@@ -29,3 +37,8 @@ export default function Nav({ authenticated, currentUser, logout }) {
     </div>
   );
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const elems = document.querySelectorAll(".dropdown-trigger");
+  const instances = M.Dropdown.init(elems, {});
+});
