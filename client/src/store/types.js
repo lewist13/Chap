d709a80d614f2dcb4ff319a17659f0a977031ece
lewist13 <lewistpro@gmail.com>
2021-01-10 14:21:47 -0500

    readded missing types

--- client/src/store/types.js
+++ client/src/store/types.js
@@ -1,3 +1,7 @@
+export const CREATE_SERVER = "CREATE_SERVER";
+export const GET_SERVER_BY_ID = "GET_SERVER_BY_ID";
+export const UPDATE_SERVER = "UPDATE_SERVER";
+export const DELETE_SERVER = "DELETE_SERVER";
 export const GET_CHANNELS = "GET_CHANNELS";
 export const GET_MESSAGES = "GET_MESSAGES";
 export const GET_SERVERS = "GET_SERVERS";
