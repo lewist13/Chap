b2dc3fdd8fd062e99ea03e2c2db988e577e0645b
lewist13 <lewistpro@gmail.com>
2021-01-10 14:13:58 -0500

    setup baseline for channels/messages

--- client/src/store/reducers/ChannelReducer.js
+++ client/src/store/reducers/ChannelReducer.js
@@ -1,3 +1,20 @@
 // store messages
 // separate reducer for message control
 // KEEP SIMPLE PLSSSSSS
+const { GET_CHANNELS, GET_MESSAGES } = require("../types");
+
+const initialState = {
+  channels: [],
+  messages: [],
+};
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case GET_CHANNELS:
+      return { ...state, channels: action.payload };
+    case GET_MESSAGES:
+      return { ...state, messages: action.payload };
+    default:
+      return { ...state };
+  }
+}
