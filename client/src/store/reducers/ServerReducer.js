70ccb254120de51447dd6ab87707c81918059f10
lewist13 <lewistpro@gmail.com>
2021-01-10 14:09:38 -0500

    cleaned up commented/useless code

--- client/src/store/reducers/ServerReducer.js
+++ client/src/store/reducers/ServerReducer.js
@@ -1,12 +1,4 @@
-const {
-  CREATE_SERVER,
-  GET_SERVER_BY_ID,
-  UPDATE_SERVER,
-  DELETE_SERVER,
-  GET_CHANNELS,
-  GET_SERVERS,
-  SET_SERVER,
-} = require("../types");
+const { GET_SERVERS, SET_SERVER } = require("../types");
 
 const initialState = {
   selectedServer: {},
@@ -19,17 +11,6 @@ export default function (state = initialState, action) {
       return { ...state, servers: action.payload };
     case SET_SERVER:
       return { ...state, selectedServer: action.payload };
-    // one server
-    // all servers
-    // case CREATE_SERVER
-    //   return { ...state, server: [...action.payload] };
-    // case GET_SERVER_BY_ID:
-    //   return { ...state, get: [...action.payload] };
-    // case DELETE_SERVER:
-    //   let filterServers = state.allServers.filter(allServers, server.id) => server.id !== action.id
-    //   return { ...state, allServers: filterServers };
-    // case GET_CHANNELS:
-    //   return { ...state, channels: [...action.payload] };
     default:
       return { ...state };
   }
